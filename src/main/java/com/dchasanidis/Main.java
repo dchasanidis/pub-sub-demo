38cf4af424e9683038b2092ad28191dd15facf6e
Dimitris Chasanidis <dchasanidis@dol-guldur.speedport.ip>
2024-05-26 19:03:08 +0200

    impl

--- src/main/java/com/dchasanidis/Main.java
+++ src/main/java/com/dchasanidis/Main.java
@@ -2,13 +2,17 @@ package com.dchasanidis;
 
 public class Main {
     public static void main(String[] args) {
+        final int subCount = 10;
+        final int messageCount = 10;
+
         Topic topic = new Topic("my first topic");
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < subCount; i++) {
             topic.addSubscriber(new SubscriberImpl("sub" + i));
         }
 
-        topic.publish(new Message("Message 1"));
-        topic.publish(new Message("Message 2"));
+        for (int i = 0; i < messageCount; i++) {
+            topic.publish(new Message("Message " + i));
+        }
 
         topic.shutDown();
     }
